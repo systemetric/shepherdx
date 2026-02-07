f2e4fd5bbfaf932d2231ba42d1c6f65b982413ad
Nathan Gill <nathan.j.gill@outlook.com>
2026-02-07 11:26:09 +0000

    dump websocket connection path

--- shepherdx/ws/ws.py
+++ shepherdx/ws/ws.py
@@ -1,6 +1,19 @@
+import asyncio
+from websockets import serve
+
 class ShepherdWebSockets:
     def __init__(self):
-        pass
+        self._conns = []
 
     def run(self):
-        pass
+        asyncio.run(self._loop())
+
+    async def _loop(self):
+        stop = asyncio.get_running_loop().create_future()
+        async with serve(self._conn_handler, "0.0.0.0", 5001) as server:
+            await stop
+
+    async def _conn_handler(self, websocket):
+        path = websocket.request.path[1::]
+        print(f"Channel: {path}")
+
