62d8acb1814ddf85c77c19761054603ea4cbafa7
Nathan Gill <nathan.j.gill@outlook.com>
2026-02-21 11:09:50 +0000

    feat(ws): read images from hopper, expose on `/camera` endpoint

--- shepherdx/ws/ws.py
+++ shepherdx/ws/ws.py
@@ -1,14 +1,18 @@
 import os
 import json
+import atexit
 import base64
 import asyncio
 import logging
+import threading
 import websockets
 import coloredlogs
 from websockets import serve
 from shepherdx.common import Config, Channels, State
 from shepherdx.common.mqtt import ShepherdMqtt
+from hopper import HopperPipe, HopperPipeType
 
+BUF_SIZE = 65536
 SHEPHERD_WS_SERVICE_ID = "shepherd-ws"
 
 class ShepherdWebSockets:
@@ -26,23 +30,62 @@ class ShepherdWebSockets:
         else:
             self._current_image = None
         self._next_image = None
+        self._camera_pipe = None
         self._logs = ""
 
     def run(self):
-        self.logger.info("Started Shepherd WS server")
+        self.logger.info("Starting Shepherd WS server")
+
+        atexit.register(self._at_exit)
+
+        self._camera_pipe = HopperPipe(HopperPipeType.OUT, SHEPHERD_WS_SERVICE_ID, Channels.camera)
+        self._camera_pipe.open()
+
+        self._image_thread = threading.Thread(target=self._load_images)
+        self._image_thread.daemon = True
+        self._image_thread.start()
+
         asyncio.run(self._loop())
 
-    async def _loop(self):
-        stop = asyncio.get_running_loop().create_future()
+    def _load_images(self):
+        """ forever load images from hopper pipe into image buffer """
+        image_buf = ""
 
+        while True:
+            buf = self._camera_pipe.read(BUF_SIZE).decode()
+            buf = buf.split("\n")   # full images terminated with newlines
+
+            for i in range(len(buf)):
+                image_buf += buf[i]
+
+                # last segment is always stored, not returned
+                if i != len(buf) - 1:
+                    self._next_image = image_buf
+                    image_buf = ""
+
+    def _at_exit(self):
+        if self._camera_pipe:
+            self._camera_pipe.close()
+
+    async def _load_next_image(self):
+        while self._next_image is None:
+            await asyncio.sleep(1)
+
+        # this is a race, but not a problematic one since valid data
+        # is still guaranteed
+        self._current_image = self._next_image
+        self._next_image = None
+
+    async def _loop(self):
         async with serve(self._conn_handler, "0.0.0.0", 5001) as server:
             async with ShepherdMqtt(SHEPHERD_WS_SERVICE_ID) as mqttc:
                 await mqttc.subscribe("#", self._mqtt_callback, None)
 
-                try:
-                    await stop  # This just runs the loops forever
-                except asyncio.CancelledError:
-                    pass
+                # forever broadcast new images from hopper
+                while True:
+                    await self._load_next_image()
+                    ws = self._collect_ws(Channels.camera)
+                    websockets.broadcast(ws, self._current_image)
 
     async def _mqtt_callback(self, topic, payload):
         if topic == Channels.shepherd_run_status:
